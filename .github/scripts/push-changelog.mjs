247c386faa2225587c2fbf817bbd150ef8241e65
and <jonkrausejams@gmail.com>
2026-05-10 13:01:25 -0400

    Fix Notion script environment variable names

--- .github/scripts/push-changelog.mjs
+++ .github/scripts/push-changelog.mjs
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
-const NOTION_TOKEN = process.env.NOTION_ACCESS_TOKEN;
+const NOTION_TOKEN = process.env.NOTION_TOKEN;
 
 /** @type {Record<string, string>} */
 const HEADERS = {
