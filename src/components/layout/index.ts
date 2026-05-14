d3cc8bd8b1d844bfb4735047ec8c3e44a2cbfdd6
Jon K <jonkrausejams@gmail.com>
2026-05-14 15:49:02 -0400

    hi

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,10 +2,11 @@ export { default as ContactSection } from './ContactSection.astro';
 export { default as CTASection } from './CTASection.astro';
 export { default as FAQSection } from './FAQSection.astro';
 export { default as FeaturesSection } from './FeaturesSection.astro';
-export { default as HeroSection } from './HeroSection.astro';
 export { default as ListSection } from './ListSection.astro';
 export { default as SearchSection } from './SearchSection.astro';
 export { default as StatsSection } from './StatsSection.astro';
-export { default as TestimonialsSection } from './TestimonialSection.astro';
+export { default as TestimonialSection } from './TestimonialSection.astro';
+export { default as HeroSection } from './HeroSection.astro';
+
 export * from './primitives';
 
---
+++ src/components/sectional/wide-pricing-section/index.ts
@@ -0,0 +1,2 @@
+export { default as PricingCard } from './PricingCard.astro';
+export { default as PricingSection } from './PricingSection.astro';
